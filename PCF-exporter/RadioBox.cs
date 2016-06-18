4eee9e73e45aa02cebeca8f89b3ea39cf41e6565
Michail Golubjev <xopkop@gmail.com>
2016-06-18 22:28:18 +0200

    Removed redudant custom radio button groups

--- PCF-exporter/RadioBox.cs
+++ PCF-exporter/RadioBox.cs
@@ -7,6 +7,8 @@ using System.Windows.Forms;
 
 namespace PCF_Functions
 {
+    //This class is introduced to fix the default behaviour of radiobuttons in RadioBox. I couldn't get the radiobuttons
+    //to switch on and off correctly inside one RadioBox. I found this solution somewhere on the net.
     public partial class RadioBox : System.Windows.Forms.GroupBox
     {
         protected override void OnControlAdded(ControlEventArgs e)
