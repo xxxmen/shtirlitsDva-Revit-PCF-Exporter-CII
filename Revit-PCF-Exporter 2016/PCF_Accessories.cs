67f39f384d701ded1b9c371d7cee1b7a355b1c23
Michail Golubjev <xopkop@gmail.com>
2016-07-11 11:40:28 +0200

    Renaming cont.

---
+++ Revit-PCF-Exporter 2016/PCF_Accessories.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using PCF_Functions;
+using PCF_Taps;
+
+using pdef = PCF_Functions.ParameterDefinition;
+using plst = PCF_Functions.ParameterList;
+
+namespace PCF_Accessories
+{
+    public class PCF_Accessories_Export
+    {
+        private IEnumerable<Element> accessoriesList;
+        private StringBuilder sbAccessories;
+        private Document doc;
+        private string key;
+        private plst pList; //An experiment to use an object instead of doing new plst() all the time
+
+        public StringBuilder Export(string pipeLineAbbreviation, IEnumerable<Element> elements, Document document)
+        {
+            doc = document;
+            key = pipeLineAbbreviation;
+            pList = new plst();
+            //paramList = new plst();
+            //The list of fittings, sorted by TYPE then SKEY
+            accessoriesList = elements.
+                OrderBy(e => e.get_Parameter(pList.PCF_ELEM_TYPE.Guid).AsString()).
+                ThenBy(e => e.get_Parameter(pList.PCF_ELEM_SKEY.Guid).AsString());
+            
+            sbAccessories = new StringBuilder();
+            foreach (Element element in accessoriesList)
+            {
+                //If the Element Type field is empty -> ignore the component
+                if (string.IsNullOrEmpty(element.get_Parameter(pList.PCF_ELEM_TYPE.Guid).AsString())) continue;
+
+                sbAccessories.Append(element.get_Parameter(pList.PCF_ELEM_TYPE.Guid).AsString());
+                sbAccessories.AppendLine();
+                sbAccessories.Append("    COMPONENT-IDENTIFIER ");
+                sbAccessories.Append(element.get_Parameter(pList.PCF_ELEM_COMPID.Guid).AsInteger());
+                sbAccessories.AppendLine();
+
+                //Cast the elements gathered by the collector to FamilyInstances
+                FamilyInstance familyInstance = (FamilyInstance)element;
+                Options options = new Options();
+                //MEPModel of the elements is accessed
+                MEPModel mepmodel = familyInstance.MEPModel;
+                //Get connector set for the element
+                ConnectorSet connectorSet = mepmodel.ConnectorManager.Connectors;
+
+                //Switch to different element type configurations
+                switch (element.get_Parameter(pList.PCF_ELEM_TYPE.Guid).AsString())
+                {
+                    case ("FILTER"):
+                        //Process endpoints of the component
+                        Connector primaryConnector = null; Connector secondaryConnector = null;
+
+                        foreach (Connector connector in connectorSet)
+                        {
+                            if (connector.GetMEPConnectorInfo().IsPrimary) primaryConnector = connector;
+                            if (connector.GetMEPConnectorInfo().IsSecondary) secondaryConnector = connector;
+                        }
+
+                        //Process endpoints of the component
+                        sbAccessories.Append(EndWriter.WriteEP1(element, primaryConnector));
+                        sbAccessories.Append(EndWriter.WriteEP2(element, secondaryConnector));
+
+                        break;
+
+                    case ("INSTRUMENT"):
+                        //Process endpoints of the component
+                        primaryConnector = null; secondaryConnector = null;
+
+                        foreach (Connector connector in connectorSet)
+                        {
+                            if (connector.GetMEPConnectorInfo().IsPrimary) primaryConnector = connector;
+                            if (connector.GetMEPConnectorInfo().IsSecondary) secondaryConnector = connector;
+                        }
+
+                        //Process endpoints of the component
+                        sbAccessories.Append(EndWriter.WriteEP1(element, primaryConnector));
+                        sbAccessories.Append(EndWriter.WriteEP2(element, secondaryConnector));
+                        sbAccessories.Append(EndWriter.WriteCP(familyInstance));
+
+                        break;
+
+                    case ("VALVE"):
+                        goto case ("INSTRUMENT");
+
+                    case ("VALVE-ANGLE"):
+                        //Process endpoints of the component
+                        primaryConnector = null; secondaryConnector = null;
+
+                        foreach (Connector connector in connectorSet)
+                        {
+                            if (connector.GetMEPConnectorInfo().IsPrimary) primaryConnector = connector;
+                            if (connector.GetMEPConnectorInfo().IsSecondary) secondaryConnector = connector;
+                        }
+
+                        //Process endpoints of the component
+                        sbAccessories.Append(EndWriter.WriteEP1(element, primaryConnector));
+                        sbAccessories.Append(EndWriter.WriteEP2(element, secondaryConnector));
+
+                        //The centre point is obtained by creating an unbound line from primary connector and projecting the secondary point on the line.
+                        XYZ reverseConnectorVector = -primaryConnector.CoordinateSystem.BasisZ;
+                        Line primaryLine = Line.CreateUnbound(primaryConnector.Origin,reverseConnectorVector);
+                        XYZ centrePoint = primaryLine.Project(secondaryConnector.Origin).XYZPoint;
+
+                        sbAccessories.Append(EndWriter.WriteCP(centrePoint));
+
+                        break;
+
+                    case ("INSTRUMENT-DIAL"):
+                        ////Process endpoints of the component
+                        //primaryConnector = null;
+
+                        //foreach (Connector connector in connectorSet) primaryConnector = connector;
+
+                        ////Process endpoints of the component
+                        //sbAccessories.Append(EndWriter.WriteEP1(element, primaryConnector));
+
+                        ////The co-ords point is obtained by creating an unbound line from primary connector and taking an arbitrary point a long the line.
+                        //reverseConnectorVector = -primaryConnector.CoordinateSystem.BasisZ.Multiply(0.656167979);
+                        //XYZ coOrdsPoint = primaryConnector.Origin;
+                        //Transform pointTranslation;
+                        //pointTranslation = Transform.CreateTranslation(reverseConnectorVector);
+                        //coOrdsPoint = pointTranslation.OfPoint(coOrdsPoint);
+
+                        primaryConnector = null;
+                        foreach (Connector connector in connectorSet) primaryConnector = connector;
+                        //Connector information extraction
+
+                        sbAccessories.Append(EndWriter.WriteEP1(element, primaryConnector));
+
+                        XYZ primConOrigin = primaryConnector.Origin;
+                        
+                        //Analyses the geometry to obtain a point opposite the main connector.
+                        //Extraction of the direction of the connector and reversing it
+                        reverseConnectorVector = -primaryConnector.CoordinateSystem.BasisZ;
+                        Line detectorLine = Line.CreateUnbound(primConOrigin, reverseConnectorVector);
+                        //Begin geometry analysis
+                        GeometryElement geometryElement = familyInstance.get_Geometry(options);
+
+                        //Prepare resulting point
+                        XYZ endPointAnalyzed = null;
+
+                        foreach (GeometryObject geometry in geometryElement)
+                        {
+                            GeometryInstance instance = geometry as GeometryInstance;
+                            if (null == instance) continue;
+                            foreach (GeometryObject instObj in instance.GetInstanceGeometry())
+                            {
+                                Solid solid = instObj as Solid;
+                                if (null == solid || 0 == solid.Faces.Size || 0 == solid.Edges.Size) continue;
+                                foreach (Face face in solid.Faces)
+                                {
+                                    IntersectionResultArray results = null;
+                                    XYZ intersection = null;
+                                    SetComparisonResult result = face.Intersect(detectorLine, out results);
+                                    if (result != SetComparisonResult.Overlap) continue;
+                                    intersection = results.get_Item(0).XYZPoint;
+                                    if (intersection.IsAlmostEqualTo(primConOrigin) == false) endPointAnalyzed = intersection;
+                                }
+                            }
+                        }
+
+                        sbAccessories.Append(EndWriter.WriteCO(endPointAnalyzed));
+
+                        break;
+
+                    case "SUPPORT":
+                        primaryConnector = (from Connector c in connectorSet where c.GetMEPConnectorInfo().IsPrimary select c).FirstOrDefault();
+                        sbAccessories.Append(EndWriter.WriteCO(familyInstance, primaryConnector));
+                        break;
+                }
+
+                Composer elemParameterComposer = new Composer();
+                sbAccessories.Append(elemParameterComposer.ElemParameterWriter(element));
+
+                #region CII export
+                Composer composer = new Composer();
+                if (!string.Equals(element.get_Parameter(pList.PCF_ELEM_TYPE.Guid).AsString(), "SUPPORT")) sbAccessories.Append(composer.CIIWriter(doc, key));
+                #endregion
+
+                sbAccessories.Append("    UNIQUE-COMPONENT-IDENTIFIER ");
+                sbAccessories.Append(element.UniqueId);
+                sbAccessories.AppendLine();
+
+                //Process tap entries of the element if any
+                //Diameter Limit nullifies the tapsWriter output if the tap diameter is less than the limit so it doesn't get exported
+                if (string.IsNullOrEmpty(element.LookupParameter("PCF_ELEM_TAP1").AsString()) == false)
+                {
+                    TapsWriter tapsWriter = new TapsWriter(element, "PCF_ELEM_TAP1", doc);
+                    sbAccessories.Append(tapsWriter.tapsWriter);
+                }
+                if (string.IsNullOrEmpty(element.LookupParameter("PCF_ELEM_TAP2").AsString()) == false)
+                {
+                    TapsWriter tapsWriter = new TapsWriter(element, "PCF_ELEM_TAP2", doc);
+                    sbAccessories.Append(tapsWriter.tapsWriter);
+                }
+                if (string.IsNullOrEmpty(element.LookupParameter("PCF_ELEM_TAP3").AsString()) == false)
+                {
+                    TapsWriter tapsWriter = new TapsWriter(element, "PCF_ELEM_TAP3", doc);
+                    sbAccessories.Append(tapsWriter.tapsWriter);
+                }
+            }
+
+            //// Clear the output file
+            //System.IO.File.WriteAllBytes(InputVars.OutputDirectoryFilePath + "Accessories.pcf", new byte[0]);
+
+            //// Write to output file
+            //using (StreamWriter w = File.AppendText(InputVars.OutputDirectoryFilePath + "Accessories.pcf"))
+            //{
+            //    w.Write(sbAccessories);
+            //    w.Close();
+            //}
+            return sbAccessories;
+        }
+    }
+}
---
+++ Revit-PCF-Exporter 2016/PCF_Supports.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using PCF_Functions;
+using BuildingCoder;
+using iv = PCF_Functions.InputVars;
+
+namespace PCF_Functions
+{
+
+    public class SetSupportPipingSystem
+    {
+        public Result Execute(ExternalCommandData commandData, ref string msg, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            Document doc = uiApp.ActiveUIDocument.Document;
+            UIDocument uidoc = uiApp.ActiveUIDocument;
+            Transaction trans = new Transaction(doc, "Set System");
+            MEPModel mepModel = null;
+
+            trans.Start();
+            try
+            {
+                //Select Element to provide a system
+                //Pipe type to restrict selection of tapping element
+                Type t = typeof(Pipe);
+
+                //Select tap element
+                Element systemDonor = Util.SelectSingleElementOfType(uidoc, t, "Select a pipe in desired system.", false);
+
+                if (systemDonor == null) throw new Exception("System assignment cancelled!");
+
+                //Select Element to add to system
+                Element elementToAdd = Util.SelectSingleElement(uidoc, "Select support to add to system.");
+
+                if (elementToAdd == null) throw new Exception("System assignment cancelled!");
+
+                //Cast the selected element to Pipe
+                Pipe pipe = (Pipe) systemDonor;
+                //Get the pipe type from pipe
+                ElementId pipeTypeId = pipe.PipeType.Id;
+
+                //Get system type from pipe
+                ConnectorSet pipeConnectors = pipe.ConnectorManager.Connectors;
+                Connector pipeConnector = (from Connector c in pipeConnectors where true select c).FirstOrDefault();
+                ElementId pipeSystemType = pipeConnector.MEPSystem.GetTypeId();
+
+                //Collect levels and select one level
+                FilteredElementCollector collector = new FilteredElementCollector(doc);
+                ElementClassFilter levelFilter = new ElementClassFilter(typeof(Level));
+                ElementId levelId = collector.WherePasses(levelFilter).FirstElementId();
+
+                //Get the connector from the support
+                FamilyInstance familyInstanceToAdd = (FamilyInstance) elementToAdd;
+                ConnectorSet connectorSetToAdd = new ConnectorSet();
+                mepModel = familyInstanceToAdd.MEPModel;
+                connectorSetToAdd = mepModel.ConnectorManager.Connectors;
+                if (connectorSetToAdd.IsEmpty)
+                    throw new Exception(
+                        "The support family lacks a connector. Please read the documentation for correct procedure of setting up a support element.");
+                Connector connectorToConnect =
+                    (from Connector c in connectorSetToAdd where true select c).FirstOrDefault();
+
+                //Create a point in space to connect the pipe
+                XYZ direction = connectorToConnect.CoordinateSystem.BasisZ.Multiply(2);
+                XYZ origin = connectorToConnect.Origin;
+                XYZ pointInSpace = origin.Add(direction);
+                
+                //Create the pipe
+                Pipe newPipe = Pipe.Create(doc, pipeTypeId, levelId, connectorToConnect, pointInSpace);
+
+                //Change the pipe system type to match the picked pipe (it is not always matching)
+                newPipe.SetSystemType(pipeSystemType);
+
+                trans.Commit();
+
+                trans.Start("Delete the pipe");
+                
+                //Delete the pipe
+                doc.Delete(newPipe.Id);
+
+                trans.Commit();
+            }
+
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                trans.RollBack();
+                return Result.Cancelled;
+            }
+
+            catch (Exception ex)
+            {
+                trans.RollBack();
+                msg = ex.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
