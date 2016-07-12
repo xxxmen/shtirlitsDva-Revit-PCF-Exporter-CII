114e768a18bcb3e2defafabe96eea897829398ee
shtirlitsDva <xopkop@gmail.com>
2016-07-12 10:48:41 +0200

    Update PCF_Output.cs

--- Revit-PCF-Exporter 2016/PCF_Output.cs
+++ Revit-PCF-Exporter 2016/PCF_Output.cs
@@ -7,11 +7,11 @@ namespace PCF_Output
 {
     public class Output
     {
-        private static StringBuilder _collect;
-        private static string _outputDir;
-        private static Document _document;
+        private StringBuilder _collect;
+        private string _outputDir;
+        private Document _document;
 
-        public static void OutputWriter(Document doc, StringBuilder collect, string outputDirectory)
+        public void OutputWriter(Document doc, StringBuilder collect, string outputDirectory)
         {
             _collect = collect; _outputDir = outputDirectory;
             _document = doc;
@@ -20,8 +20,8 @@ namespace PCF_Output
             string dateAndTime = DateTime.Now.ToString();
             dateAndTime = dateAndTime.Replace(" ", "_");
             dateAndTime = dateAndTime.Replace(":", "-");
-            //string filename = _outputDir+"\\" + docName + "_" + dateAndTime + ".pcf";
-            string filename = _outputDir+"\\" + docName + ".pcf";
+            string filename = _outputDir+"\\" + docName + "_" + dateAndTime + ".pcf";
+            //string filename = _outputDir+"\\" + docName + ".pcf";
 
             //Clear the output file
             System.IO.File.WriteAllBytes(filename, new byte[0]);
