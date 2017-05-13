1fb282337950480d4db08426b099b10eec16dbe0
Michail Golubjev <xopkop@gmail.com>
2017-05-13 22:40:08 +0200

    Small start

--- Revit-NTR-Exporter/NTR_Main.cs
+++ Revit-NTR-Exporter/NTR_Main.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using NTR_Functions;
 
 namespace NTR_Exporter
 {
     class NTR_Exporter
     {
-
+        public NTR_Exporter(ExternalCommandData cData)
+        {
+            ConfigurationData conf = new ConfigurationData(cData);
+            
+        }
     }
 }
