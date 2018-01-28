c20a1227faac52b9581f5a8d2d4d2dfe7e21742f
Michail Golubjev <xopkop@gmail.com>
2018-01-28 22:45:01 +0100

    Continue 01

--- revit-cii-neutral-exporter/CIIN_Exporter_form.Designer.cs
+++ revit-cii-neutral-exporter/CIIN_Exporter_form.Designer.cs
@@ -252,7 +252,7 @@ namespace CIINExporter
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(162, 46);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Delete PCF parameters";
+            this.button2.Text = "Delete CIIN parameters";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -262,7 +262,7 @@ namespace CIINExporter
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(162, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Import PCF parameters";
+            this.button1.Text = "Import CIIN parameters";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
